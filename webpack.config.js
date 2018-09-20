7359f416e56b0f5a803e2bdc6904ab3d83123e32
Vladislav <vladpivovarov2797@ya.ru>
2018-09-20 23:34:19 +0300

    add babel settings from .babelrc to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,8 @@ module.exports = {
       {
         test: /\.js$/,
         loader: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        query: {"presets": ["env"]},
       },
       {
         test:/\.scss$/,
